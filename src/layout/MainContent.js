0ca90714c50bd5ff8ba1d4c9a85da04003863036
jcmann <jen.c.mann@gmail.com>
2021-09-28 23:10:55 -0500

    Setup project structure and add basic layout components

---
+++ src/layout/MainContent.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const MainContent = (props) => {
+  return (
+    <main>
+      <h2>Main</h2>
+    </main>
+  );
+};
---
+++ src/layout/SideBar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Sidebar = (props) => {
+  return (
+    <aside>
+      <h2>Aside</h2>
+    </aside>
+  );
+};

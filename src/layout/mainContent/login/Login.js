12eb8e6f6157c27ca8e818c4fe77c3c6bd183600
Jen <jen.c.mann@gmail.com>
2021-10-14 17:39:39 -0500

    Add bootstrap-styled login form with username and password field

--- src/layout/mainContent/login/Login.js
+++ src/layout/mainContent/login/Login.js
@@ -1,3 +1,36 @@
 export const Login = () => {
-  return <h2>Login</h2>;
+  return (
+    <>
+      <h2>Log In</h2>
+      <form>
+        <div className="form-group">
+          <label for="username">
+            Username:
+            <input
+              type="text"
+              name="username"
+              id="username"
+              className="form-control"
+              required
+            />
+          </label>
+        </div>
+
+        <div className="form-group">
+          <label for="password">
+            Password:
+            <input
+              type="password"
+              name="password"
+              id="password"
+              className="form-control"
+              required
+            />
+          </label>
+        </div>
+
+        <input type="submit" className="btn btn-primary" value="Log In" />
+      </form>
+    </>
+  );
 };

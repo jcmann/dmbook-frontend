223dbafd1c5f0d899123525bc69a6904a6893e27
jcmann <jen.c.mann@gmail.com>
2021-10-13 19:47:02 -0500

    Add rough tool menu

--- src/layout/SideBar.js
+++ src/layout/SideBar.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { MainMenu } from "./MainMenu";
+import { ToolMenu } from "./ToolMenu";
 
 export const Sidebar = (props) => {
   return (
     <aside>
       <MainMenu />
+      <ToolMenu />
     </aside>
   );
 };
---
+++ src/layout/ToolMenu.js
@@ -0,0 +1,7 @@
+export const ToolMenu = (props) => {
+  return (
+    <section>
+      <button>New :)</button>
+    </section>
+  );
+};

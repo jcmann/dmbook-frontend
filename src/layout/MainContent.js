e39d81aafcf2a02e3dad8a2eed5751295694cf7b
Jen <jen.c.mann@gmail.com>
2021-10-14 13:42:14 -0500

    Add basic home main content component

--- src/layout/MainContent.js
+++ src/layout/MainContent.js
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux';
 
 import { Characters } from './mainContent/characters/Characters';
 import { Encounters } from './mainContent/encounters/Encounters';
+import { Home } from './mainContent/home/Home';
 
 const styles = {
   border: '1px solid black',
@@ -16,6 +17,7 @@ export const MainContent = (props) => {
 
   return (
     <main className={'col-sm-8'} style={styles}>
+      {currentContent === 'home' && <Home />}
       {currentContent === 'characters' && <Characters />}
       {currentContent === 'encounters' && <Encounters />}
     </main>

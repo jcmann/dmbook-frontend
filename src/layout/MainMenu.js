ff630bde3a34331912e3649b5a92b9eaddbafc68
Jen <jen.c.mann@gmail.com>
2021-10-23 15:40:04 -0500

    Remove LoggedOutMenu because Amplify Auth is implemented

--- src/layout/MainMenu.js
+++ src/layout/MainMenu.js
@@ -3,11 +3,9 @@ import LoggedInMenu from './LoggedInMenu';
 import { useSelector } from 'react-redux';
 
 export const MainMenu = (props) => {
-  const isLoggedIn = useSelector((state) => state.login.isLoggedIn);
   return (
     <nav>
-      {isLoggedIn && <LoggedInMenu />}
-      {!isLoggedIn && <LoggedOutMenu />}
+      <LoggedInMenu />
     </nav>
   );
 };

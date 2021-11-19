efa79fdda27b1aab5aafe634c3bbd6d537be36ae
Jen <jen.c.mann@gmail.com>
2021-11-18 20:38:23 -0600

    Updates

--- build/static/js/21.6a4b4212.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpdmbook-frontend"]=this["webpackJsonpdmbook-frontend"]||[]).push([[21],{209:function(n,t,e){"use strict";e.r(t),e.d(t,"amplify_container",(function(){return o}));var i=e(11),o=function(){function n(n){Object(i.k)(this,n)}return n.prototype.render=function(){return Object(i.i)(i.b,null,Object(i.i)("slot",null))},n}();o.style="amplify-container{display:-ms-flexbox;display:flex;-ms-flex-align:center;align-items:center;-ms-flex-pack:center;justify-content:center;height:100vh}"}}]);
-//# sourceMappingURL=21.6a4b4212.chunk.js.map

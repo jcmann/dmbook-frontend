673c2242211bb5d1f4d3431cfe5fbd45a81c47ca
Jen Mann <jen.c.mann@gmail.com>
2021-11-24 13:26:22 -0600

    Restructure and set config

--- src/config/config.js
+++ src/config/config.js
@@ -2,6 +2,5 @@
  * Whatever data is in this file is used throughout the application to facilitate standardized URLs across
  * development (localhost) and deployment (live links).
  */
-export const API_BASE_URL = "";
-export const USER_CHARACTERS_ENDPOINT = "";
-export const USER_ENCOUNTERS_ENDPOINT = "";
+export const API_BASE_URL = "http://localhost:8080/DMBook_war/api/";
+export const USERS_ENDPOINT = API_BASE_URL + "users/";
--- src/config/prodConfig.js
+++ src/config/prodConfig.js
@@ -3,5 +3,4 @@
  * but it should be copy-pasted into the config.js file in this directory as necessary when building to deploy.
  */
 export const API_BASE_URL = "";
-export const USER_CHARACTERS_ENDPOINT = "";
-export const USER_ENCOUNTERS_ENDPOINT = "";
+export const USERS_ENDPOINT = API_BASE_URL + "users/";

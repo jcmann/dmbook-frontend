75512ca308086d8e7fc40f03f4ab1b94f2715862
Jen Mann <jen.c.mann@gmail.com>
2021-11-24 13:46:59 -0600

    Add thunk and extraReducers

--- src/redux/dataSlice.js
+++ src/redux/dataSlice.js
@@ -1,8 +1,24 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { API_BASE_URL, USERS_ENDPOINT } from "../config/config";
 
-export const getAllResourcesThunk = createAsyncThunk("api/getAll", async () => {
-  // Get stuff from API
-});
+/**
+ * getAllResources expects the following information passed in as the first argument: the logged-in user's ID
+ * Token JWT from auth info, and the endpoint being requested. This information forms the URL that the
+ * request is sent to.
+ */
+export const getAllResourcesThunk = createAsyncThunk(
+  "api/getAll",
+  async (arg, { dispatch, getState, signal }) => {
+    let URL = API_BASE_URL + arg.jwt + USERS_ENDPOINT;
+    const response = await fetch(URL)
+      .then((data) => {
+        return data.json();
+      })
+      .then((jsonData) => {
+        console.log(jsonData);
+      });
+  }
+);
 
 /**
  * This slice handles all connectivity to the API
@@ -11,5 +27,11 @@ export const dataSlice = createSlice({
   name: "dataSlice",
   initialState: {},
   reducers: {},
-  extraReducers: {},
+  extraReducers: {
+    [getAllResourcesThunk.fulfilled](state, { payload }) {
+      state.resources = payload;
+    },
+  },
 });
+
+export default dataSlice.reducer;

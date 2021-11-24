9b3af07d718313f30f4195e7f9d131d850d57303
Jen Mann <jen.c.mann@gmail.com>
2021-11-23 23:18:24 -0600

    Add barebones dataSlice file

---
+++ src/redux/dataSlice.js
@@ -0,0 +1,11 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+/**
+ * This slice handles all connectivity to the API
+ */
+export const dataSlice = createSlice({
+  name: "dataSlice",
+  initialState: {},
+  reducers: {},
+  extraReducers: {},
+});

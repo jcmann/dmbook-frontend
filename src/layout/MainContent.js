0014681125a9690461c3f0b190f05dbfe3ea4aac
Jen Mann <jen.c.mann@gmail.com>
2021-11-28 11:38:58 -0600

    Add edit routing

--- src/layout/MainContent.js
+++ src/layout/MainContent.js
@@ -1,30 +1,35 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { useSelector } from "react-redux";
 
-import { Characters } from './mainContent/characters/Characters';
-import AddNewCharacter from './mainContent/characters/AddNewCharacter';
-import { CreateAccount } from './mainContent/createAccount/CreateAccount';
-import { Encounters } from './mainContent/encounters/Encounters';
-import AddNewEncounter from './mainContent/encounters/AddNewEncounter';
-import { Home } from './mainContent/home/Home';
-import { Login } from './mainContent/login/Login';
+import { Characters } from "./mainContent/characters/Characters";
+import AddNewCharacter from "./mainContent/characters/AddNewCharacter";
+import { CreateAccount } from "./mainContent/createAccount/CreateAccount";
+import { Encounters } from "./mainContent/encounters/Encounters";
+import AddNewEncounter from "./mainContent/encounters/AddNewEncounter";
+import { Home } from "./mainContent/home/Home";
+import { Login } from "./mainContent/login/Login";
 
 const styles = {
-  border: '1px solid black',
+  border: "1px solid black",
 };
 
 export const MainContent = (props) => {
   const currentContent = useSelector((state) => state.display.currentContent);
 
   return (
-    <main className={'col-sm-8'} style={styles}>
-      {currentContent === 'home' && <Home />}
-      {currentContent === 'login' && <Login />}
-      {currentContent === 'createAccount' && <CreateAccount />}
-      {currentContent === 'characters' && <Characters />}
-      {currentContent === 'characters:add' && <AddNewCharacter />}
-      {currentContent === 'encounters' && <Encounters />}
-      {currentContent === 'encounters:add' && <AddNewEncounter />}
+    <main className={"col-sm-8"} style={styles}>
+      {currentContent === "home" && <Home />}
+      {currentContent === "login" && <Login />}
+      {currentContent === "createAccount" && <CreateAccount />}
+      {currentContent === "characters" && <Characters />}
+      {currentContent === "characters:add" && (
+        <AddNewCharacter formType="add" />
+      )}
+      {currentContent === "characters:edit" && (
+        <AddNewCharacter formType="edit" />
+      )}
+      {currentContent === "encounters" && <Encounters />}
+      {currentContent === "encounters:add" && <AddNewEncounter />}
     </main>
   );
 };

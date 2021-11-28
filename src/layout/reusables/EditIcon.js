d62e0ec6d355094194673db6c61ca3ff9a059cec
Jen Mann <jen.c.mann@gmail.com>
2021-11-28 13:50:06 -0600

    Edit icon now routes to form

--- src/layout/reusables/EditIcon.js
+++ src/layout/reusables/EditIcon.js
@@ -1,9 +1,26 @@
-import './EditIcon.css';
+import { useDispatch, useSelector } from "react-redux";
+import { changeCurrentContent } from "../../redux/displaySlice";
+import { editResourceThunk } from "../../redux/dataSlice";
 
+import "./EditIcon.css";
+
+/**
+ * Expects props itemID and resourceType
+ */
 const EditIcon = (props) => {
+  const dispatch = useDispatch();
+  const userData = useSelector((state) => state.login.authInfo.user);
+
+  const editClickHandler = (event) => {
+    event.preventDefault();
+    dispatch(
+      changeCurrentContent({ newContent: `${props.resourceType}:edit` })
+    );
+  };
+
   return (
     <>
-      <a className="dmbookIcon" href="">
+      <a className="dmbookIcon" href="" onClick={editClickHandler}>
         <i className="fas fa-edit"></i>
       </a>
     </>

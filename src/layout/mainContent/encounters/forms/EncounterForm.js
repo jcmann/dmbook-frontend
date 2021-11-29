e2958d63f6f11dc6f179b8873c59a7c9c84063a8
Jen Mann <jen.c.mann@gmail.com>
2021-11-28 18:48:52 -0600

    Encounter form now displays the current user's characters in a list

--- src/layout/mainContent/encounters/forms/EncounterForm.js
+++ src/layout/mainContent/encounters/forms/EncounterForm.js
@@ -4,6 +4,7 @@ import Select from "react-select";
 import { useDispatch, useSelector } from "react-redux";
 
 import { changeEditingStatuses } from "../../../../redux/dataSlice";
+import { USERS_ENDPOINT } from "../../../../config/config";
 
 const required = (value) => (value ? undefined : "Required");
 
@@ -18,8 +19,32 @@ const mockMonsters = [
 const EncounterForm = (props) => {
   const dispatch = useDispatch();
   const [selectedOption, setSelectedOption] = useState(null);
+  const userData = useSelector((state) => state.login.authInfo.user);
   const resourceData = useSelector((state) => state.data.resources);
   const currentlyEditing = useSelector((state) => state.data.editResourceID);
+
+  let characterData = []; // should the uE just be an IIF?
+
+  // Runs when component first mounts
+  useEffect(async () => {
+    // query database for characters belonging to this user
+    let URL =
+      USERS_ENDPOINT +
+      userData.signInUserSession.idToken.jwtToken +
+      "/characters";
+    characterData = await fetch(URL)
+      .then((data) => {
+        return data.json();
+      })
+      .then((dataJSON) => {
+        console.log("USER CHARACTERS: ");
+        console.log(dataJSON);
+        return dataJSON;
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
 
   // An immediately invoked function to assign initial form values based on state
   let formData = (() => {
@@ -36,7 +61,7 @@ const EncounterForm = (props) => {
     return () => {
       dispatch(changeEditingStatuses({ isEditing: false, editResourceID: 0 }));
     };
-  });
+  }, []);
 
   return (
     <div>
@@ -73,20 +98,20 @@ const EncounterForm = (props) => {
                 </div>
               )}
             </Field>
-            {/*<div className="form-group">*/}
-            {/*  <label>Characters:</label>*/}
-            {/*  <Field*/}
-            {/*    name="characters"*/}
-            {/*    component="select"*/}
-            {/*    className="form-control"*/}
-            {/*    multiple*/}
-            {/*  >*/}
-            {/*    <option value="halfling">Character 1</option>*/}
-            {/*    <option value="elf">Character 2</option>*/}
-            {/*    <option value="dragonborn">Character 3</option>*/}
-            {/*  </Field>*/}
-            {/*</div>*/}
-            {/*<Field name="monsters" validate={required}>*/}
+            <div className="form-group">
+              <label>Characters:</label>
+              <Field
+                name="characters"
+                component="select"
+                className="form-control"
+                multiple
+              >
+                {characterData.map((current) => {
+                  return <option value={current.name}>{current.name}</option>;
+                })}
+              </Field>
+            </div>
+            {/*<Field name="monsters">*/}
             {/*  {({ input, meta }) => (*/}
             {/*    <div className="form-group">*/}
             {/*      <label>Monsters:</label>*/}

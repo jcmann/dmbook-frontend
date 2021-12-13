4b9079c429932c33490a455af4dc6dc30ef2a81d
Jen Mann <jen.c.mann@gmail.com>
2021-12-13 15:52:15 -0600

    Update configs

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 /**
- * Whatever data is in this file is used throughout the application to facilitate standardized URLs across
- * development (localhost) and deployment (live links).
+ * This file will contain all URLs, etc., to be used for local development and not in prod. Low-tech,
+ * but it should be copy-pasted into the config.js file in this directory as necessary when deploying or developing.
  */
 export const API_BASE_URL = "http://localhost:8080/DMBook_war/api/";
 export const USERS_ENDPOINT = API_BASE_URL + "users/";
--- src/config/prodConfig.js
+++ src/config/prodConfig.js
@@ -2,5 +2,6 @@
  * This file will contain all URLs, etc., to be used  in prod. Low-tech,
  * but it should be copy-pasted into the config.js file in this directory as necessary when building to deploy.
  */
-export const API_BASE_URL = "";
+export const API_BASE_URL =
+  "http://dmbookapi-env.eba-kdb2efih.us-east-2.elasticbeanstalk.com/api/";
 export const USERS_ENDPOINT = API_BASE_URL + "users/";
